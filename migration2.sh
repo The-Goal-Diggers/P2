0df2857f428063d59b0a471eb3bd6ca600176ee0
Ubuntu <alexander@test1.pdvv2nkzrezuvowuadtau4knxh.jx.internal.cloudapp.net>
2020-04-04 18:46:00 +0000

    migration script fix

--- migration2.sh
+++ migration2.sh
@@ -1,5 +1,5 @@
 # Create new migration
-dotnet ef migrations add migration$1 -s GoalDigger/GoalDigger.API/GoalDigger.API.csproj -c GoalDiggerDBContext -p GoalDigger/GoalDigger.DataStore/GoalDigger.DataStore.csproj
+dotnet ef migrations add migration$1 -s GoalDigger/GoalDigger.API/GoalDigger.API.csproj -c GoalDiggerDBContext -p GoalDigger/GoalDigger.API/GoalDigger.API.csproj
 # Apply migration to the database
-dotnet ef database update migration$1 -s GoalDigger/GoalDigger.API/GoalDigger.API.csproj -c GoalDiggerDBContext -p GoalDigger/GoalDigger.DataStore/GoalDigger.DataStore.csproj
+dotnet ef database update migration$1 -s GoalDigger/GoalDigger.API/GoalDigger.API.csproj -c GoalDiggerDBContext -p GoalDigger/GoalDigger.API/GoalDigger.API.csproj
 
