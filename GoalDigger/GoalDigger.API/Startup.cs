07b2887b23b6868c964fb5920340711eb4fe6645
Alexander Barnard <alexwb1@live.com>
2020-04-04 19:22:55 -0500

    testing db

--- GoalDigger/GoalDigger.API/Startup.cs
+++ GoalDigger/GoalDigger.API/Startup.cs
@@ -10,9 +10,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 // using Microsoft.Extensions.Logging;
-using Microsoft.EntityFrameworkCore;
-using GoalDigger.DataStore.Databases;
-using GoalDigger.DataStore.Repositories;
+// using Microsoft.EntityFrameworkCore;
+// using GoalDigger.DataStore.Databases;
+// using GoalDigger.DataStore.Repositories;
 
 namespace GoalDigger.API
 {
@@ -28,19 +28,19 @@ namespace GoalDigger.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<GoalDiggerDBContext>(opt =>
-               opt.UseSqlServer(Configuration.GetConnectionString("main")));
-            //    services.AddDbContext<GoalDiggerDBContext>.UseSqlServer("main");
-            services.AddScoped<PostRepository>(); // transient vs scoped vs singleton
-            services.AddScoped<UserRepository>();
+            // services.AddDbContext<GoalDiggerDBContext>(opt =>
+            //    opt.UseSqlServer(Configuration.GetConnectionString("main")));
+            // //    services.AddDbContext<GoalDiggerDBContext>.UseSqlServer("main");
+            // services.AddScoped<PostRepository>(); // transient vs scoped vs singleton
+            // services.AddScoped<UserRepository>();
 
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, GoalDiggerDBContext goaldigger_context)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env/*, GoalDiggerDBContext goaldigger_context*/)
         {
-            goaldigger_context.Database.Migrate();
+            // goaldigger_context.Database.Migrate();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
