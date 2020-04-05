aa66980be6f8faaa5312add7b95c5e9ba49e373c
Alexander Barnard <alexwb1@live.com>
2020-04-04 20:34:04 -0500

    added repositories

--- GoalDigger/GoalDigger.API/Repositories/UserRepository.cs
+++ GoalDigger/GoalDigger.API/Repositories/UserRepository.cs
@@ -1,125 +1,125 @@
-// using System;
-// using System.Collections.Generic;
-// using System.Linq;
-// using Microsoft.EntityFrameworkCore;
-// // using GoalDigger.Domain.Interfaces;
-// using GoalDigger.Domain.Models;
-// using GoalDigger.DataStore.Databases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+// using GoalDigger.Domain.Interfaces;
+using GoalDigger.Domain.Models;
+using GoalDigger.DataStore.Databases;
 
-// namespace GoalDigger.DataStore.Repositories 
-// {
-//   public class UserRepository
-//   {
+namespace GoalDigger.DataStore.Repositories 
+{
+  public class UserRepository
+  {
 
-//     private GoalDiggerDBContext _db;
+    private GoalDiggerDBContext _db;
     
-//     public UserRepository(GoalDiggerDBContext dbContext)
-//     {
-//       _db = dbContext;
-//     }
+    public UserRepository(GoalDiggerDBContext dbContext)
+    {
+      _db = dbContext;
+    }
 
-//     public List<UserModel> Read() 
-//     {
-// 			return _db.Users.ToList();
-//     }
+    public List<UserModel> Read() 
+    {
+			return _db.Users.ToList();
+    }
 
-//     // public List<Order> Get(User user)
-//     // {
-//     //   List<Order> list = (_db.Order.Where(o => o.UserId == user.UserId).ToList());
-//     //   return list;
-//     // }
+    // public List<Order> Get(User user)
+    // {
+    //   List<Order> list = (_db.Order.Where(o => o.UserId == user.UserId).ToList());
+    //   return list;
+    // }
 
-//     // public List<Post> Get(long userId)
-//     // {
-//     //   List<Order> list = (_db.Order.Where(o => o.UserId == userId).ToList());
-//     //   return list;
-//     // }
+    // public List<Post> Get(long userId)
+    // {
+    //   List<Order> list = (_db.Order.Where(o => o.UserId == userId).ToList());
+    //   return list;
+    // }
 
-//     // public List<Order> GetPeriod(Store store, double days)
-//     // {
-//     //   List<Order> list = (_db.Order.Where(o => o.StoreId == store.StoreId && o.Date.AddHours(days*24) >= DateTime.Now).ToList());
+    // public List<Order> GetPeriod(Store store, double days)
+    // {
+    //   List<Order> list = (_db.Order.Where(o => o.StoreId == store.StoreId && o.Date.AddHours(days*24) >= DateTime.Now).ToList());
       
-//     //   return list;
-//     // }
+    //   return list;
+    // }
 
-//     // public List<Order> GetPeriod(User user, double days)
-//     // {
-//     //   List<Order> list = (_db.Order.Where(o => o.UserId == user.UserId && o.Date.AddHours(days*24) >= DateTime.Now).ToList());
+    // public List<Order> GetPeriod(User user, double days)
+    // {
+    //   List<Order> list = (_db.Order.Where(o => o.UserId == user.UserId && o.Date.AddHours(days*24) >= DateTime.Now).ToList());
 
-//     //   return list;
-//     // }
+    //   return list;
+    // }
 
-//     // public int Get2Hours(User user)
-//     // {
-//     //   List<Order> list = Get(user);
-//     //   int count = 0;
+    // public int Get2Hours(User user)
+    // {
+    //   List<Order> list = Get(user);
+    //   int count = 0;
 
-//     //   foreach (var o in list)
-//     //   {
-//     //       double minutes = DateTime.Now.Subtract(o.Date).TotalMinutes;
+    //   foreach (var o in list)
+    //   {
+    //       double minutes = DateTime.Now.Subtract(o.Date).TotalMinutes;
 
-//     //       if (minutes < 2*60)
-//     //       {
-//     //         count++;
-//     //       }
-//     //   }
-//     //   return count;
-//     // }
+    //       if (minutes < 2*60)
+    //       {
+    //         count++;
+    //       }
+    //   }
+    //   return count;
+    // }
 
-//     // public int Get24HoursAtStore(User user, Store store)
-//     // {
-//     //   List<Order> list = (_db.Order.Where(o => o.UserId == user.UserId && o.StoreId != store.StoreId).ToList());
-//     //   int count = 0;
-//     //   foreach (var o in list)
-//     //   {
-//     //       double minutes = DateTime.Now.Subtract(o.Date).TotalMinutes;
+    // public int Get24HoursAtStore(User user, Store store)
+    // {
+    //   List<Order> list = (_db.Order.Where(o => o.UserId == user.UserId && o.StoreId != store.StoreId).ToList());
+    //   int count = 0;
+    //   foreach (var o in list)
+    //   {
+    //       double minutes = DateTime.Now.Subtract(o.Date).TotalMinutes;
 
-//     //       if (minutes < 24*60)
-//     //       {
-//     //         count++;
-//     //       }
-//     //   }
-//     //   return count;
-//     // }
+    //       if (minutes < 24*60)
+    //       {
+    //         count++;
+    //       }
+    //   }
+    //   return count;
+    // }
 
-//     // public bool Post(Order order)
-//     // {
-//     //   _db.Order.Add(order);
-//     //   return _db.SaveChanges() == 1;
-//     // }
+    // public bool Post(Order order)
+    // {
+    //   _db.Order.Add(order);
+    //   return _db.SaveChanges() == 1;
+    // }
 
-//     // public bool HasItBeenMoreThan2Hours(User user)
-//     // {
-//     //   bool check = true;
-//     //   List<Order> list = Get(user);
-//     //   foreach (var o in list)
-//     //   {
+    // public bool HasItBeenMoreThan2Hours(User user)
+    // {
+    //   bool check = true;
+    //   List<Order> list = Get(user);
+    //   foreach (var o in list)
+    //   {
         
-//     //     DateTime date = DateTime.Now;
-//     //     double minutes = date.Subtract(o.Date).TotalMinutes;
+    //     DateTime date = DateTime.Now;
+    //     double minutes = date.Subtract(o.Date).TotalMinutes;
 
-//     //     if (minutes < 2*60)
-//     //     {
-//     //       check = false;
-//     //     }
-//     //   }
-//     //   return check;
-//     // } 
+    //     if (minutes < 2*60)
+    //     {
+    //       check = false;
+    //     }
+    //   }
+    //   return check;
+    // } 
 
-//     // public bool HasItBeenMoreThan24Hours(User user, Store store)
-//     // {
-//     //   bool check = true;
-//     //   List<Order> list = Get(user);
-//     //   if (list.Count() > 0)
-//     //   {
-//     //     int count = Get24HoursAtStore(user, store);
-//     //     if (count > 0)
-//     //     {
-//     //       check = false;
-//     //     }
-//     //   }
-//     //   return check;
-//     // }
+    // public bool HasItBeenMoreThan24Hours(User user, Store store)
+    // {
+    //   bool check = true;
+    //   List<Order> list = Get(user);
+    //   if (list.Count() > 0)
+    //   {
+    //     int count = Get24HoursAtStore(user, store);
+    //     if (count > 0)
+    //     {
+    //       check = false;
+    //     }
+    //   }
+    //   return check;
+    // }
 
-//   }
-// }
+  }
+}
