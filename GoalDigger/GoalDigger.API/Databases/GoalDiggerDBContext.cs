34de06e92f92eada29812ff32fd654a3fdae2d15
Alexander Barnard <alexwb1@live.com>
2020-04-04 19:33:55 -0500

    testing with no DB

--- GoalDigger/GoalDigger.API/Databases/GoalDiggerDBContext.cs
+++ GoalDigger/GoalDigger.API/Databases/GoalDiggerDBContext.cs
@@ -1,37 +1,37 @@
-using Microsoft.EntityFrameworkCore;
-using GoalDigger.Domain.Models;
+// using Microsoft.EntityFrameworkCore;
+// using GoalDigger.Domain.Models;
 
-namespace GoalDigger.DataStore.Databases
-{
-    public class GoalDiggerDBContext : DbContext
-    {
-        public DbSet<PostModel> Posts { get; set; }
-        public DbSet<UserModel> Users { get; set; }
+// namespace GoalDigger.DataStore.Databases
+// {
+//     public class GoalDiggerDBContext : DbContext
+//     {
+//         public DbSet<PostModel> Posts { get; set; }
+//         public DbSet<UserModel> Users { get; set; }
 
-        public GoalDiggerDBContext(DbContextOptions<GoalDiggerDBContext> options)
-            : base(options) {}
+//         public GoalDiggerDBContext(DbContextOptions<GoalDiggerDBContext> options)
+//             : base(options) {}
 
 
-        protected override void OnModelCreating(ModelBuilder builder) 
-        {
-            // Set keys
-            builder.Entity<PostModel>().HasKey(x => x.uid);
-            builder.Entity<UserModel>().HasKey(x => x.uid);
+//         protected override void OnModelCreating(ModelBuilder builder) 
+//         {
+//             // Set keys
+//             builder.Entity<PostModel>().HasKey(x => x.uid);
+//             builder.Entity<UserModel>().HasKey(x => x.uid);
 
-            // Define entity relationships
-            builder.Entity<UserModel>().HasMany(x => x.Posts).WithOne(x => x.User);
+//             // Define entity relationships
+//             builder.Entity<UserModel>().HasMany(x => x.Posts).WithOne(x => x.User);
 
-            // Input dummy data
-            builder.Entity<UserModel>().HasData(new UserModel[]
-            {
-                new UserModel() { UserName = "Alex1234" },
-            });
+//             // Input dummy data
+//             builder.Entity<UserModel>().HasData(new UserModel[]
+//             {
+//                 new UserModel() { UserName = "Alex1234" },
+//             });
 
-        }
+//         }
 
         
 
-    }
-}
+//     }
+// }
 
 
