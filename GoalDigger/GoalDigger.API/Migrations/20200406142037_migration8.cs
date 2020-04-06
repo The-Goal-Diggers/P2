7d9219143f1c755bc2fb80c037200918c8fd2ae5
Isaiah Dummer <ildummer@uwm.edu>
2020-04-06 09:28:52 -0500

    add style

---
+++ GoalDigger/GoalDigger.API/Migrations/20200406142037_migration8.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GoalDigger.API.Migrations
+{
+    public partial class migration8 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
