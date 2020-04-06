62ec1dfc959833169be7d9245689e4081fd441fc
nick <nick.bush@zohomail.com>
2020-04-05 21:14:06 -0500

    add login

--- GoalDigger/GoalDigger.MVC/Controllers/HomeController.cs
+++ GoalDigger/GoalDigger.MVC/Controllers/HomeController.cs
@@ -9,29 +9,39 @@ using GoalDigger.MVC.Models;
 
 namespace GoalDigger.MVC.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      _logger = logger;
     }
+
+        [HttpGet]
+    public IActionResult Login()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public IActionResult Login(UserViewModel user)
+    {
+      //get users from db. and run login. 
+
+      //on successful login, show post index page
+      return View("post/index/");
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
 }
--- GoalDigger/GoalDigger.MVC/Startup.cs
+++ GoalDigger/GoalDigger.MVC/Startup.cs
@@ -50,7 +50,7 @@ namespace GoalDigger.MVC
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Login}/{id?}");
             });
         }
     }
